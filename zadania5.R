b49165495427bbb3fcbedae24de607795d815cfd
a-wroblewska <38754530+a-wroblewska@users.noreply.github.com>
2018-05-17 15:27:26 +0200

    Create zadania5.R

---
+++ zadania5.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(dplyr)
+
+# Zad 5-1
+
+library(readr)
+movies <- read_csv("C:/Users/admin/Desktop/movies.csv")
+View(movies)
+
+# Zad 5-2
+
+filter(movies, Comedy, year == 2005)
+
+# Zad 5-3
+
+select(movies, title, year, budget) %>% 
+  arrange(desc(budget))
+
+# Zad 5-4
+
+movies %>% select(Animation, year) %>% filter(year == 1990)
+
+# Zad 5-5
+
+movies %>% select(Drama, length) %>% arrange(desc(length))
+
+
+
